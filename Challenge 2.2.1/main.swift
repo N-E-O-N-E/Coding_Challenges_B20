598ed8f3649933d0ca5235a29202defe0e153d24
Markus Wirtz <info@markuswirtz.net>
2024-07-02 11:24:14 +0200

    Erste Challenge heute

---
+++ Challenge 2.2.1/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Challenge 2.2.1
+//
+//  Created by Markus Wirtz on 02.07.24.
+//
+
+
+import Foundation
+
+
+var steps: Int = 0
+repeat {
+    steps += 1
+    print(steps)
+} while (steps != 100)
+
+
+var steps2: Int = 0
+while steps2 <= 100 {
+    print(steps2)
+    steps2 += 1
+}
+
+ 
+
