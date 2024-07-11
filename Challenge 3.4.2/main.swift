0d769469d6fde9194e2c5e36f757cdb560c3b37a
Markus Wirtz <info@markuswirtz.net>
2024-07-11 12:06:09 +0200

    Challenge 3.4.2

--- Challenge 3.4.2/main.swift
+++ Challenge 3.4.2/main.swift
@@ -7,5 +7,39 @@
 
 import Foundation
 
-print("Hello, World!")
+struct hotel {
+    
+    var nameHotel: String
+    let anzZimmer: Int
+    
+    static var zimmerNr: Int = 0
+    static var anzZimmerBelegt = 14
+    
+    mutating func checkIn(Gast: String, Zimmer: Int) {
+        hotel.zimmerNr = Zimmer
+        hotel.anzZimmerBelegt += 1
+
+    }
+    
+    static func getZimmer() {
+        print("Es sind aktuell '\(hotel.anzZimmerBelegt) Zimmer' belegt!")
+    }
+    
+    mutating func checkOut(Zimmer: Int) {
+        hotel.zimmerNr = 0
+        hotel.anzZimmerBelegt -= 1
+    }
+    
+    init(nameHotel: String, anzZimmer: Int) {
+        self.nameHotel = nameHotel
+        self.anzZimmer = anzZimmer
+    }
+    
+}
+
+var neuesHotel = hotel(nameHotel: "Musterhotel", anzZimmer: 100)
+
+print("Das Hotel '\(neuesHotel.nameHotel)' hat '\(neuesHotel.anzZimmer) Zimmer'")
+hotel.getZimmer()
+
 
