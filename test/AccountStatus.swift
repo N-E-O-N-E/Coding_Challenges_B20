82ea0e816e1c7dfcc4e87cf0d32d4533ed4c3ef3
Markus Wirtz <info@markuswirtz.net>
2024-07-12 10:09:33 +0200

    update

---
+++ test/AccountStatus.swift
@@ -0,0 +1,16 @@
+//
+//  AccountStatus.swift
+//  test
+//
+//  Created by Markus Wirtz on 12.07.24.
+//
+
+import Foundation
+
+enum AccountStatus{
+    
+    case Basic
+    case Standard
+    case Premium
+    
+}
