f9e43d41becb0168c00bc189986d5e5b4ed720e3
Markus Wirtz <info@markuswirtz.net>
2024-10-11 11:30:28 +0200

    Datensicherung von NEONE

---
+++ Modul_3_iOS/CodingChallenges_iOS/Challenge_03_07_03_1.swift
@@ -0,0 +1,43 @@
+//
+//  Challenge_03_07_03_1.swift
+//  CodingChallenges_iOS
+//
+//  Created by Markus Wirtz on 10.10.24.
+//
+
+/*
+
+Deine Aufgabe ist es, für folgende Implementierungen zu entscheiden, ob sie in das ViewModel oder in das Repository gehören:
+
+ 
+Abrufen der Informationen über Haustiere von einem Server
+ Respository -> OK
+
+Speichern eines neuen Haustiers in der lokalen Datenbank
+ ViewModel
+
+Berechnen des Alters eines Haustiers basierend auf dem Geburtsdatum
+ ViewModel
+
+Laden von Bildern der Haustiere aus einer Remote-Datenquelle
+ Respository
+
+Filterung der Haustiere nach Typ (z.B. Hund, Katze)
+ ViewModel
+
+Überprüfen, ob der Nutzer eingeloggt ist, bevor Haustierdaten geladen werden
+ ViewModel
+
+Validieren von Nutzereingaben beim Anlegen eines neuen Haustiers (z.B. Name darf nicht leer sein)
+ ViewModel
+
+Formatieren des letzten Tierarztbesuchsdatums zur Anzeige im UI
+ ViewModel
+
+Berechnen, wann der nächste Tierarztbesuch fällig ist, basieren auf dem letzten Besuch
+ ViewModel
+
+Berechnen, ob ein Haustier in einem bestimmten Zeitraum genügend Bewegung erhalten hat (z.B. mindestens 60 Minuten am Tag)
+ ViewModel
+ 
+*/
