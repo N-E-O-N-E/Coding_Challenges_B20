7427290437e47624e445227fb42da17fc85efc56
Markus Wirtz <info@markuswirtz.net>
2024-10-14 13:25:01 +0200

    Datensicherung von NEONE

---
+++ Modul_3_iOS/CodingChallenges_iOS/Challenge_03_09_01_1.swift
@@ -0,0 +1,31 @@
+//
+//  Challenge_03_09_01_1.swift
+//  CodingChallenges_iOS
+//
+//  Created by Markus Wirtz on 14.10.24.
+//
+
+import Foundation
+
+/*
+
+Welche Anbieter können in eine iOS-App zur Authentifizierung integriert werden?
+ Facebook, Apple, Twitter, GitHub, Microsoft, Yahoo, GameCenter, via Telefonnummer, OpenID Connect
+ 
+Welche 5 Vorteile bietet Cloud Firestore?
+ Einfaches speichern, synchronisieren und abfragen dank NoSQL
+ Erstellen serverloser Anwendungen mit Cloud Functions
+ Cloud Firestore lässt sich auch in die Firebase-Authentifizierung integrieren
+ Synchronisieren der Daten auf verschiedenen Geräten, online oder offline
+ Globale Skalierung die mit dem Unternehmenswachstum mitwächst
+ 
+ Wozu kann Cloud Storage genutzt werden?
+ Um Bilder, Audio-, Video- oder andere benutzergenerierte Inhalte zu speichern.
+ 
+Inwiefern können in Firebase gespeicherte Daten abgesichert werden?
+ Schreiben Sie benutzerdefinierte Regeln, Granularität
+ 
+Kann Swift auch genutzt werden, um Cloud Functions zu schreiben?
+ JavaScript-, TypeScript- oder Python-Code schreiben um Ereignisse aus Firebase zu verarbeiten,
+
+*/
